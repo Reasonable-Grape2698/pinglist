6820858fc8b260ca1c4a5b2794d738a6d71642e5
Reasonable-Grape2698 <dyl.secure@gmail.com>
2024-12-08 18:55:04 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -1,4 +1,4 @@
 cat list.txt| tail -n +4 |  while read output
 do
-    echo $output":" && ping -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }'
+    echo $output":" && ping -4 -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }'
 done
