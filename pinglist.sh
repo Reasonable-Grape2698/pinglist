00c64286e5b7177de5026b8cc5a1ff75b49af292
Reasonable-Grape2698 <dyl.secure@gmail.com>
2024-12-08 19:16:52 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -1,4 +1,10 @@
 cat list.txt| tail -n +4 |  while read output
 do
-    echo $output":" $(ping -4 -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }')
+    export ping=$(ping -4 -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
+    echo $output":" $ping" ms"
+    export total=$(echo "$total+$ping" | bc)
+    export count=$(echo "$count+1" | bc)
 done
+
+echo "Total: " $total
+echo "Average: " $(echo "$total/$count" | bc)
