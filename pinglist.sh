bce3a6da525eb6ab81097f76d75c570117e779b8
Reasonable-Grape2698 <dyl.secure@gmail.com>
2024-12-08 19:39:13 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -1,12 +1,19 @@
 export total=0
 export count=0
+export min=999
+export max=0
 while read output
 do
     export ping=$(ping -4 -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5:"FAIL") }')
     echo $output":" $ping" ms"
     export total=$(echo "$total+$ping" | bc)
     export count=$(echo "$count+1" | bc)
+    if (($ping < $min))
+    then
+        min=$ping
+    if (($ping > $max))
+    then
+        max=$ping 
 done << (cat list.txt| tail -n +4)
 
-echo "Total: " $total
-echo "Average: " $(echo "$total/$count" | bc)
+echo "min/avg/max/total" $min"/"$(echo "$total/$count" | bc)""/""$max"/""$total"
