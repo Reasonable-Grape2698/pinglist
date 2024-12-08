489893db010970d1a770a9a26cece402a2498dd3
Reasonable-Grape2698 <dyl.secure@gmail.com>
2024-12-08 18:47:36 +1100

    Update pinglist.sh

--- pinglist.sh
+++ pinglist.sh
@@ -1,15 +1,4 @@
-#!/bin/sh
-#Program name: pinglist.sh
-#Contact is John Mintz.  john.mintz@gmail.com twitter@johnwmintz
-#download this file and the list.txt file and place in the same folder when executing script.
-#Place list of all IPs below the # lines.
-
 cat list.txt| tail -n +4 |  while read output
 do
-    ping -c 1 "$output" > /dev/null
-    if [ $? -eq 0 ]; then
-    echo "device $output is up" 
-    else
-    echo "device $output is down"
-    fi
+    echo $output":" && ping -qc1 $(echo $output | cut -d "#" -f 1) 2>&1 | awk -F'/' 'END{ print (/^rtt/? $5" ms":"FAIL") }'
 done
